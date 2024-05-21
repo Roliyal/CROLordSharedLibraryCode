5d930e52de3c8d814ff0ec86f9442f4ab664b7ba
Rich_13 <106301413+Rich-13@users.noreply.github.com>
2024-05-21 18:01:33 +0800

    Update SonarQubeScanner.groovy

--- src/org/devops/SonarQubeScanner.groovy
+++ src/org/devops/SonarQubeScanner.groovy
@@ -26,7 +26,7 @@ class SonarQubeScanner {
             }
         }
 
-        // 判断SonarQube质量门是否通过
+        // 判断SonarQube是否通过
         script.echo "Checking SonarQube quality gate"
         script.withCredentials([script.string(credentialsId: 'sonar', variable: 'SONAR_TOKEN')]) {
             def authHeader = "Basic " + (script.SONAR_TOKEN + ":").bytes.encodeBase64().toString().trim()
