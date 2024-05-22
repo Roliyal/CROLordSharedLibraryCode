f78605c7b11ba300b99290f9b5480f1956d77cf6
Rich_13 <106301413+Rich-13@users.noreply.github.com>
2024-05-22 15:44:37 +0800

    Update BuildUtils.groovy

--- src/org/devops/BuildUtils.groovy
+++ src/org/devops/BuildUtils.groovy
@@ -1,28 +1,49 @@
 package org.devops
 
 class BuildUtils {
-    static void buildImage(script, String platform, Map envVars) {
-        // 使用提供的环境变量和平台信息执行实际的构建步骤
-        script.unstash 'source-code'
 
-        // 根据平台选择容器
-        String containerName = platform == 'linux/amd64' ? 'kanikoamd' : 'kanikoarm'
-        script.container(containerName) {
-            script.sh """
-                /kaniko/executor \
-                  --context ${envVars.WORKSPACE}/${envVars.BUILD_DIRECTORY} \
-                  --dockerfile ${envVars.BUILD_DIRECTORY}/Dockerfile \
-                  --destination ${envVars.IMAGE_REGISTRY}/${envVars.IMAGE_NAMESPACE}/${envVars.REPOSITORY_NAME}:${envVars.VERSION_TAG}-${platform.replace('/', '-')} \
-                  --cache=true \
-                  --cache-repo=${envVars.IMAGE_REGISTRY}/${envVars.IMAGE_NAMESPACE}/cache \
-                  --skip-tls-verify \
-                  --skip-unused-stages=true \
-                  --custom-platform=${platform} \
-                  --build-arg BUILDKIT_INLINE_CACHE=1 \
-                  --snapshotMode=redo \
-                  --log-format=text \
-                  --verbosity=info
-            """
+    static void buildImage(script, String platform, Map envVars) {
+        switch(platform) {
+            case 'linux/amd64':
+                script.sh 
+                    sh """
+                        kaniko \
+                        --context ${env.WORKSPACE}/${params.BUILD_DIRECTORY} \
+                        --dockerfile ${params.BUILD_DIRECTORY}/Dockerfile \
+                        --destination ${env.IMAGE_REGISTRY}/${env.IMAGE_NAMESPACE}/${env.JOB_NAME}:${VERSION_TAG}-amd64 \
+                        --cache=true \
+                        --cache-repo=${env.IMAGE_REGISTRY}/${env.IMAGE_NAMESPACE}/cache \
+                        --skip-tls-verify \
+                        --skip-unused-stages=true \
+                        --custom-platform=linux/amd64 \
+                        --build-arg BUILDKIT_INLINE_CACHE=1 \
+                        --snapshot-mode=redo \
+                        --log-format=text \
+                        --verbosity=info
+                        """
+                break
+            case 'linux/arm64':
+                script.sh 
+                    sh """
+                        kaniko \
+                        --context ${env.WORKSPACE}/${params.BUILD_DIRECTORY} \
+                        --dockerfile ${params.BUILD_DIRECTORY}/Dockerfile \
+                        --destination ${env.IMAGE_REGISTRY}/${env.IMAGE_NAMESPACE}/${env.JOB_NAME}:${VERSION_TAG}-arm64 \
+                        --cache=true \
+                        --cache-repo=${env.IMAGE_REGISTRY}/${env.IMAGE_NAMESPACE}/cache \
+                        --skip-tls-verify \
+                        --skip-unused-stages=true \
+                        --custom-platform=linux/arm64 \
+                        --build-arg BUILDKIT_INLINE_CACHE=1 \
+                        --snapshot-mode=redo \
+                        --log-format=text \
+                        --verbosity=info
+                        """
+                break
+            default:
+                throw new RuntimeException("Unsupported platform: ${platform}")
         }
     }
+
 }
+
